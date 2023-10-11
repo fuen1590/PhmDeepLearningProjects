0dbef2915324a0b9f0a2e453e85c282d85fd53af
fuen <925378271@qq.com>
2023-10-11 20:03:28 +0800

    同样修改用于查看不同epoch的特征的t-SNE图

--- models/RULPrediction/CNN_GRU.py
+++ models/RULPrediction/CNN_GRU.py
@@ -1,6 +1,9 @@
 import torch
 import torch.nn as nn
+import numpy as np
 
+import matplotlib.pyplot as plt
+import sklearn.manifold as manifold
 import models.RULPrediction.ContrastiveModules
 from dataset import cmapss
 from train import TrainableModule
@@ -16,7 +19,11 @@ class CnnGru(ContrastiveModel):
                  window_size,
                  model_flag="CnnGru",
                  device="cuda"):
-        super(CnnGru, self).__init__(model_flag, device="cuda")
+        super(CnnGru, self).__init__(model_flag, device=device)
+        self.tsne = None
+        self.visual_samples = None
+        self.embedding = []
+        self.epoch_num = 0
         self.convs = nn.Sequential(
             nn.Conv1d(in_channels=window_size, out_channels=16, kernel_size=10, stride=1, padding="same"),
             nn.ReLU(),
@@ -52,6 +59,16 @@ class CnnGru(ContrastiveModel):
             out = self.linears(feature_pos)
             return out, feature_pos, feature_pos_aug, feature_neg, neg_weights
 
+    def set_visual_samples(self, samples):
+        """
+        Sets the visualization samples used in epoch_start.
+
+        :param samples: (batch, len, features)
+        :return:
+        """
+        self.visual_samples = samples
+        self.tsne = manifold.TSNE(n_components=2, random_state=2023)
+
     def feature_extractor(self, x):
         # x.shape = (batch, length, features)
         batch, l, f = x.shape
@@ -62,15 +79,44 @@ class CnnGru(ContrastiveModel):
         fea = x_conv+x_grus
         return fea
 
+    def epoch_start(self):
+        if self.visual_samples is not None:
+            print("Visualizing samples processing...")
+            features = self.feature_extractor(self.visual_samples)
+            features = features.cpu().detach().numpy()
+            embedding = self.tsne.fit_transform(features)
+            self.embedding.append(embedding)
+            # plt.figure(dpi=600)
+            # plt.scatter(embedding[:, 0], embedding[:, 1], c=plt.cm.Spectral(range(len(embedding))))
+            # plt.title("Epoch:{}".format(self.epoch_num))
+            # plt.savefig(self.get_model_result_path()+"visual_embedding_{}.png".format(self.epoch_num))
+            self.epoch_num += 1
+        else:
+            print("Visualizing samples is None, ignored.")
+
+    def train_end(self):
+        plt.figure(dpi=600)
+        plt.title("Total")
+        index = 0
+        emd_index = [0, len(self.embedding)//2, len(self.embedding)-1]
+        for i in emd_index:
+            plt.scatter(self.embedding[i][:, 0], self.embedding[i][:, 1],
+                        c=plt.cm.tab20(index),
+                        edgecolors=plt.cm.Wistia(range(len(self.embedding[i][:, 0]))),
+                        label="epoch: {}".format(i))
+            index+=1
+        plt.legend()
+        plt.savefig(self.get_model_result_path() + "total_embedding.png")
+
 
 if __name__ == '__main__':
     from dataset.cmapss import Cmapss
     window_size = 32
     batch_size = 256
     threshold = 125
-    neg_samples = 4
-    subset = cmapss.Subset.FD001
-    model_flag = "RUL-1DCNN_GRU-w{}-batch{}-thresh{}-{}-neg{}-Contra-2". \
+    neg_samples = 3
+    subset = cmapss.Subset.FD004
+    model_flag = "RUL-1DCNN_GRU-w{}-batch{}-thresh{}-{}-neg{}-Contra-3". \
         format(window_size,
                batch_size,
                threshold,
@@ -84,11 +130,13 @@ if __name__ == '__main__':
                                                rul_threshold=threshold,
                                                label_norm=True,
                                                val_ratio=0.3)
-    net = CnnGru(len(cmapss.DEFAULT_SENSORS), window_size, model_flag, device="cuda")
+    net = CnnGru(len(cmapss.DEFAULT_SENSORS), window_size, model_flag, device="cuda:1")
+    visual_samples = torch.tensor(train.data[np.where(train.ids == 1)], dtype=torch.float32, device="cuda:1")
+    net.set_visual_samples(visual_samples)
     sampler = cmapss.CmapssNegativeSampler(train, neg_nums=neg_samples)
     net.prepare_data(train, test, val, batch_size=batch_size, num_workers=1)
     net.train_model(epoch=100,
                     lr=0.001,
                     criterion=models.RULPrediction.ContrastiveModules.MSEContrastiveLoss(),
                     early_stop=5,
-                    lr_lambda=lambda epoch: 10 ** -(epoch // 5))
+                    lr_lambda=lambda epoch: 10 ** -(epoch // 15))
