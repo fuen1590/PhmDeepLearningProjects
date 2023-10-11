f63bb850400d90b58147c7c8131ac2f784e896ec
fuen <925378271@qq.com>
2023-10-11 20:02:32 +0800

    新增训练过程中的回调函数，在每次迭代通过visual_samples构建可用于观察特征变化的方法，并在训练结束时绘制不同epoch下的特征t-SNE图

--- models/RULPrediction/ResBlockModel.py
+++ models/RULPrediction/ResBlockModel.py
@@ -1,4 +1,9 @@
+import torch
 import torch.nn as nn
+import numpy as np
+
+import matplotlib.pyplot as plt
+import sklearn.manifold as manifold
 
 import dataset.cmapss as cmapss
 from models.RULPrediction.ContrastiveModules import ContrastiveModel, MSEContrastiveLoss
@@ -8,6 +13,10 @@ class ResNet(ContrastiveModel):
     def __init__(self, in_features, window_size,
                  model_flag="ContrastiveResNet", device="cuda"):
         super(ResNet, self).__init__(model_flag=model_flag, device=device)
+        self.tsne = None
+        self.visual_samples = None
+        self.embedding = []
+        self.epoch_num = 0
         self.conv = nn.Conv1d(in_channels=in_features, out_channels=64, kernel_size=3, stride=1, padding=1)
         self.norm1 = nn.BatchNorm1d(64)
         self.res1 = nn.Sequential(
@@ -69,14 +78,54 @@ class ResNet(ContrastiveModel):
         flat = self.flatten(x4)
         return flat
 
+    def set_visual_samples(self, samples):
+        """
+        Sets the visualization samples used in epoch_start.
+
+        :param samples: (batch, len, features)
+        :return:
+        """
+        self.visual_samples = samples
+        self.visual_samples = torch.transpose(self.visual_samples, -1, -2)
+        self.tsne = manifold.TSNE(n_components=2, random_state=2023)
+
+    def epoch_start(self):
+        if self.visual_samples is not None:
+            print("Visualizing samples processing...")
+            features = self.feature_extractor(self.visual_samples)
+            features = features.cpu().detach().numpy()
+            embedding = self.tsne.fit_transform(features)
+            self.embedding.append(embedding)
+            # plt.figure(dpi=600)
+            # plt.scatter(embedding[:, 0], embedding[:, 1], c=plt.cm.Spectral(range(len(embedding))))
+            # plt.title("Epoch:{}".format(self.epoch_num))
+            # plt.savefig(self.get_model_result_path()+"visual_embedding_{}.png".format(self.epoch_num))
+            self.epoch_num += 1
+        else:
+            print("Visualizing samples is None, ignored.")
+
+    def train_end(self):
+        plt.figure(dpi=600)
+        plt.title("Total")
+        index = 0
+        emd_index = [0, len(self.embedding)//2, len(self.embedding)-1]
+        for i in emd_index:
+            plt.scatter(self.embedding[i][:, 0], self.embedding[i][:, 1],
+                        c=plt.cm.tab20(index),
+                        edgecolors=plt.cm.Wistia(range(len(self.embedding[i][:, 0]))),
+                        label="epoch: {}".format(i))
+            index+=1
+        plt.legend()
+        plt.savefig(self.get_model_result_path() + "total_embedding.png")
+
 
 if __name__ == '__main__':
     window_size = 32
     batch_size = 256
     threshold = 125
-    neg_samples = 4
-    subset = cmapss.Subset.FD001
-    model_flag = "RUL-ContrastiveResNet-BatchNorm-w{}-batch{}-thresh{}-{}-neg{}-dim64-withoutContra-2". \
+    neg_samples = 3
+    subset = cmapss.Subset.FD004
+    model_flag = "RUL-ContrastiveResNet-BatchNorm-w{}-batch{}-thresh{}-{}-neg{}-dim64-Contra-3". \
         format(window_size,
                batch_size,
                threshold,
@@ -90,14 +139,17 @@ if __name__ == '__main__':
                                                rul_threshold=threshold,
                                                label_norm=True,
                                                val_ratio=0.3)
-    net = ResNet(in_features=len(cmapss.DEFAULT_SENSORS), window_size=window_size, model_flag=model_flag)
-    # sampler = cmapss.CmapssNegativeSampler(train, neg_nums=neg_samples)
+    net = ResNet(in_features=len(cmapss.DEFAULT_SENSORS), window_size=window_size, model_flag=model_flag,
+                 device="cuda:0")
+    visual_samples = torch.tensor(train.data[np.where(train.ids == 1)], dtype=torch.float32).to("cuda:0")
+    net.set_visual_samples(visual_samples)
+    sampler = cmapss.CmapssNegativeSampler(train, neg_nums=neg_samples)
     net.prepare_data(train, test, val, batch_size=batch_size, num_workers=0, eval_shuffle=False)
-    net.train_model(epoch=1,
-                    lr=1e-4,
+    net.train_model(epoch=100,
+                    lr=1e-3,
                     criterion=MSEContrastiveLoss(),
                     optimizer="adam",
-                    lr_lambda=lambda x: 10 ** -(x // 5),
+                    lr_lambda=lambda x: 10 ** -(x // 15),
                     early_stop=5,
                     show_batch_loss=False)
     import sklearn.metrics as m
