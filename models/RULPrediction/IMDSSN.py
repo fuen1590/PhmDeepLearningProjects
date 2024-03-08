1d4a138e7a949f14a6cdf65d90f1a7074123e016
fuen <925378271@qq.com>
2024-03-08 16:44:32 +0800

    Formatting the IMDSSN.py

--- models/RULPrediction/IMDSSN.py
+++ models/RULPrediction/IMDSSN.py
@@ -1,31 +1,6 @@
 """
-This code is based on huggingface,
-https://github.com/huggingface/pytorch-openai-transformer-lm/blob/master/model_pytorch.py
-
-MIT License
-
-Copyright (c) 2018 OpenAI
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OFS CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-
+Implementation of https://doi.org/10.1016/j.ress.2023.109096
 """
-# Arxiv Link https://arxiv.org/pdf/1907.00235.pdf
 
 
 import numpy as np
